251ddc03b83734bb4a83c814a969cfb5edfd1857
Joël Ammann <joel@simplywing.ch>
2023-11-14 09:25:36 +0100

    update

---
+++ joam/src/main/java/ch/hslu/oop/joam/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ch.hslu.oop.joam;
+
+/**
+ *
+ * @author Joël Ammann
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
